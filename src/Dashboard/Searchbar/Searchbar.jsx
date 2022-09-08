931b9981722a81e3331fe6c5f9c6cffe952346dd
Concetta Chimienti <104746976+c-chimie@users.noreply.github.com>
2022-09-08 10:21:19 -0400

    first commit

---
+++ src/Dashboard/Searchbar/Searchbar.jsx
@@ -0,0 +1,21 @@
+import { PureComponent } from "react";
+import "./Searchbar.css";
+
+export default class Searchbar extends PureComponent {
+  handleText = (event) => {
+    this.props.handleChange(event);
+  };
+  render() {
+    return (
+      <div className="search-input">
+        <div>
+          <input
+            className="input-text"
+            placeholder="Search for an emoji..."
+            onChange={this.handleText}
+          />
+        </div>
+      </div>
+    );
+  }
+}
